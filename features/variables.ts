9689f0aaf5f37ad2840a657b44f537a25554c2a9
carlosescuderfolch <carlos.escuder.folch@es.ey.com>
2021-10-29 16:53:49 +0200

    multiple types using |

--- features/variables.ts
+++ features/variables.ts
@@ -25,7 +25,27 @@ const logNumber: (i: number) => void = (i: number) => {
 };
 
 //when to use annotations:
-// function that returns any type
+// function that returns type any
 const json = '{"x":10, "y":20}';
-const coordinates = JSON.parse(json);
+const coordinates: { x: number; y: number } = JSON.parse(json);
 console.log(coordinates);
+
+//when declare a variable in one line and initialize later
+let words = ['red', 'green', 'blue'];
+let foundWord: boolean;
+
+for (let i = 0; i < words.length; i++) {
+  if (words[i] == 'green') {
+    foundWord = true;
+    break;
+  }
+}
+
+// Variable whose type cannot be inferred
+let numbers2 = [-10, -1, 12];
+let numberAboveZero: boolean | number = false;
+for (let i = 0; i < numbers2.length; i++) {
+  if (numbers2[i] > 0) {
+    numberAboveZero = numbers2[i];
+  }
+}

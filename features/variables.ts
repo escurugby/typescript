8470158e625875467815ef291deac1e9320a2625
carlosescuderfolch <carlos.escuder.folch@es.ey.com>
2021-10-29 16:25:07 +0200

    different varibles types

---
+++ features/variables.ts
@@ -0,0 +1,24 @@
+let apples: number = 5;
+let speed: string = 'fast';
+let hasName: boolean = true;
+let nothingMuch: null = null;
+let nothing: undefined = undefined;
+
+let now: Date = new Date();
+
+let colors: string[] = ['red', 'green', 'blue'];
+let numbers: number[] = [1, 2, 3];
+let test: boolean[] = [true, false, true];
+
+class Car {}
+
+let car: Car = new Car();
+
+let point: { x: number; y: number } = {
+  x: 20,
+  y: 10,
+};
+
+const logNumber: (i: number) => void = (i: number) => {
+  console.log(i);
+};

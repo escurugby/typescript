6a5f21ba77a4cf44c447e0246f6e3eb3ed987031
carlosescuderfolch <carlos.escuder.folch@es.ey.com>
2021-10-30 21:03:36 +0200

    types in functions

--- features/functions.ts
+++ features/functions.ts
@@ -9,3 +9,41 @@ function divide(a: number, b: number): number {
 const multiply = function (a: number, b: number): number {
   return a * b;
 };
+
+const logger = (message: string): void => {
+  return console.log(message);
+};
+
+//NEVER return anything, throw error.
+const throwError = (message: string): never => {
+  throw new Error(message);
+};
+
+const throwError2 = (message: string): string => {
+  if (message) {
+    throw new Error(message);
+  }
+
+  return message;
+};
+
+const forecast = {
+  date: new Date(),
+  weather: 'sunny',
+};
+
+const logWeather = (forecast: { date: Date; weather: string }): void => {
+  console.log(forecast.date);
+  console.log(forecast.weather);
+};
+
+const logWeather2 = ({
+  date,
+  weather,
+}: {
+  date: Date;
+  weather: string;
+}): void => {
+  console.log(date);
+  console.log(weather);
+};

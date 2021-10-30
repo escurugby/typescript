d9a99d669428aed9f44c5c23d7be643b7586bdd5
carlosescuderfolch <carlos.escuder.folch@es.ey.com>
2021-10-30 21:22:50 +0200

    typing arrays

---
+++ features/arrays.ts
@@ -0,0 +1,6 @@
+const carMakers: string[] = ['ford', 'toyota', 'bmw'];
+//if declared empty, needed typing
+const carTest: string[] = [];
+
+const carsByMake = [['f150'], ['corola'], ['camaro']];
+const carsByMake2: string[][] = [];
